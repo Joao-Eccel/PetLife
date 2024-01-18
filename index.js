69c1810981d0de4e39c535d4dc2704e9ccac89a0
João Otávio Eccel <139034975+Joao-Eccel@users.noreply.github.com>
2024-01-17 21:39:55 -0300

    Colocado os itens: html, CSS e JS

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+var duvidas = document.querySelectorAll('.duvida')
+
+duvidas.forEach( 
+    function (duvida) {
+        duvida.addEventListener('click', function () {
+            duvida.classList.toggle('ativa')
+        })
+ }
+)
